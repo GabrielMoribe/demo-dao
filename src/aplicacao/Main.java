523205d72404785ab9181b35b9df428b7ef79c33
Gabriel_Moribe <163215540+gabriel-mrb@users.noreply.github.com>
2025-03-30 17:32:32 -0300

    criacao da classe Seller

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,10 +1,15 @@
 package aplicacao;
 
 import entidades.Department;
+import entidades.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        Department obj = new Department(1 , "IT");
+        Seller seller = new Seller(21,"bob","bob@email.com",new Date(),3000.0,obj);
+        System.out.println(seller);
     }
 }
---
+++ src/entidades/Seller.java
@@ -0,0 +1,91 @@
+package entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Seller implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthdate;
+    private Double baseSalary;
+    // DEPENDENCIA
+    private Department department;
+
+
+    public Seller(){}
+    public Seller(Integer id, String name, String email, Date birthdate, Double baseSalary, Department department) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Date getBirthdate() {
+        return birthdate;
+    }
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+
+    @Override public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Seller other = (Seller) obj;
+        if (id == null) {
+            if(other.id != null)
+                return false;
+        }
+        else if(!id.equals(other.id)){
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Seller [name=").append(name).append(", id=").append(id).append(", email=").append(email)
+                .append(", birthdate=").append(birthdate).append(", baseSalary=").append(baseSalary).append("]");
+        return sb.toString();
+    }
+}
+
+
